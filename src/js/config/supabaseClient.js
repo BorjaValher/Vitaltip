fd6c777ed9d2addaa881f3f81d359dfbcee65ac3
Borja Valverde <borja.valher@usal.es>
2026-06-03 21:08:32 +0200

    Replace Supabase credentials with placeholders

--- src/js/config/supabaseClient.js
+++ src/js/config/supabaseClient.js
@@ -2,7 +2,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // Reemplaza esto con los datos de tu proyecto Supabase
-const SUPABASE_URL = 'https://qoyruiizcrabniplquoz.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFveXJ1aWl6Y3JhYm5pcGxxdW96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNDkxNDEsImV4cCI6MjA5NDYyNTE0MX0.6vPoJUWWAWW9pOrhlq6kpkKb6Aka0wKtuNl_8KaPxH8';
+const SUPABASE_URL = 'x';
+const SUPABASE_KEY = 'x';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);

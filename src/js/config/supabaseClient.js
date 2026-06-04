e91923e77cab2a0305606230a687116a455cef9b
Borja Valverde <borja.valher@usal.es>
2026-06-04 15:43:04 +0200

    Update supabaseClient.js

--- src/js/config/supabaseClient.js
+++ src/js/config/supabaseClient.js
@@ -1,7 +1,6 @@
 // src/js/config/supabaseClient.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-// Reemplaza esto con los datos de tu proyecto Supabase
 const SUPABASE_URL = 'x';
 const SUPABASE_KEY = 'x';
 

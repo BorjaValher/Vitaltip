db9df0969b0a90d563005a418ee8cf55f45e4591
BorjaValher <borja.valher@usal.es>
2026-06-06 02:11:14 +0200

    Ajuste Supabase

--- src/js/config/supabaseClient.js
+++ src/js/config/supabaseClient.js
@@ -1,7 +1,8 @@
-// src/js/config/supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const SUPABASE_URL = 'x';
-const SUPABASE_KEY = 'x';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+// 2. Metemos los datos a pelo y fuera líos
+const SUPABASE_URL = 'https://qoyruiizcrabniplquoz.supabase.co'
+const SUPABASE_KEY = 'sb_publishable_6-m8-b7Cg7ylSQrhLb_HWg_epKFwcbi'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
